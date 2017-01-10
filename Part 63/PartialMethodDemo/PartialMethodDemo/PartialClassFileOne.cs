d9570ec9206d16643d6f605099e0d191ccf306be
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-10 18:26:46 +0600

    C#

---
+++ Part 63/PartialMethodDemo/PartialMethodDemo/PartialClassFileOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialMethodDemo
+{
+    public partial class SamplePartialClass
+    {
+        partial void SamplePartialMethod();
+        partial void SamplePartialMethod()
+        {
+            Console.WriteLine("SamplePartialMethod invoked.");
+        }
+        public void PublicMethod()
+        {
+            Console.WriteLine("PublicMethod Invoked.");
+
+            SamplePartialMethod();
+        }
+    }
+}
