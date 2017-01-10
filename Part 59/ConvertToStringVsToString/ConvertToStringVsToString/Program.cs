3c4895dee32046f92f498819859e922dacb7474e
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-10 18:25:08 +0600

    C#

---
+++ Part 59/ConvertToStringVsToString/ConvertToStringVsToString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        Customer C1 = null;
+         Console.WriteLine(C1.ToString());
+       // Console.WriteLine(Convert.ToString(C1));
+
+        Console.ReadLine();
+    }
+}
+public class Customer
+{
+    public string Name { get; set; }
+}
---
+++ Part 60/StringVsTextStringBuilder/StringVsTextStringBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace StringVsTextStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string userStr = "Try to ";
+            //userStr += "Learning ";
+            //userStr += "C# ";
+            //userStr += "Console ";
+            //userStr += "Application.";
+
+            //StringBuilder userStr = new StringBuilder("Try to ");
+            //userStr.Append("Learning ");
+            //userStr.Append("C# ");
+            //userStr.Append("Console ");
+            //userStr.Append("Application.");
+
+            string userStr = string.Empty;
+            for(int i = 0; i <= 1000; i++)
+            {
+                userStr += i.ToString() + " ";
+            }
+
+            Console.WriteLine(userStr);
+            Console.ReadLine();
+        }
+    }
+}
