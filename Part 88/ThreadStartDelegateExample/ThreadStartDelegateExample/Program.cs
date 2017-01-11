08743d851500514922ee982673c82765bd895d1d
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-11 23:15:23 +0600

    C#

---
+++ Part 88/ThreadStartDelegateExample/ThreadStartDelegateExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadStartDelegateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread T1 = new Thread(new ThreadStart(Number.PrintNumber));
+            T1.Start();
+        }
+    }
+    class Number
+    {
+        public static void PrintNumber()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Part 89/ThreadStartDelegateExample2/ThreadStartDelegateExample2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadStartDelegateExample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter the target number");
+            object target = Console.ReadLine();
+
+            // Create an instance ParameterizedThreadStart delegate
+            ParameterizedThreadStart parameterizedThreadStart =
+                new ParameterizedThreadStart(Number.PrintNumbers);
+            Thread T1 = new Thread(parameterizedThreadStart);
+            // Pass the traget number to the start function, which
+            // will then be passed automatically to PrintNumbers() function
+            T1.Start(target);
+        }
+    }
+
+    class Number
+    {
+        public static void PrintNumbers(object target)
+        {
+            int number = 0;
+            if (int.TryParse(target.ToString(), out number))
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
